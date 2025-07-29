a502d8849b30a3fa0ad7a6af88f2495cfccc9b05
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-07-29 11:06:57 -0600

    Creacion del proyecto

---
+++ src/main/java/com/example/demo/Practica24072025Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica24072025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica24072025Application.class, args);
+	}
+
+}
